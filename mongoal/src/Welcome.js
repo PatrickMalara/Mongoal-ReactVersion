561b98f76e6b4a35a2d11185454019ee56fb3b21
PatrickMalara <pat_malara@outlook.com>
2020-06-16 17:35:45 -0400

    Applied some style to the Welcome page

--- mongoal/src/Welcome.js
+++ mongoal/src/Welcome.js
@@ -1,38 +1,59 @@
 import React from 'react';
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	Link,
-	Redirect,
-	useHistory,
-	useLocation
-} from "react-router-dom";
-import Goals from './Goals';
-import Insights from './Insights';
- 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 class Welcome extends React.Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {redirect: null};
 	}
 
 	loginHandle = () => {
 		this.props.loginHandle();
 	}	
-	onSubmit = () => {
-		this.state.redirect = "goals"; 
-	}
 
 	render() {
 		return(
 			<div>
-				<h1>Welcome Page</h1>
-				<form>
-					<button onClick={this.loginHandle}>Login</button>	
-				</form>
-			</div>
+                <div class="white-section">
+                    <h1><strong>Mongoal</strong></h1>
+                    <h3>A goal setting app that gives you statistics</h3>
+                </div>
+                <div class="blue-section">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h2><strong>Sign Up</strong></h2>
+                            <form>
+                                <div class="form-group">
+                                    <label>Email</label>
+                                    <input type="email" class="form-control" id="signupEmail" />
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input type="password" class="form-control" id="signupPass" />
+                                </div>
+                                <button type="submit" class="btn btn-light">Sign Up</button>
+                            </form>
+                        </div>
+                        <div class="col-md-6">
+                            <h2><strong>Login</strong></h2>
+                            <form>
+                                <div class="form-group">
+                                    <label>Email</label>
+                                    <input type="email" class="form-control" id="loginEmail" />
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input type="password" class="form-control" id="loginPass" />
+                                </div>
+                                <button onClick={this.loginHandle} type="submit" class="btn btn-light">Login</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="white-section">
+                    <h6>Developed by Patrick Malara</h6>
+                </div>
+            </div>
 		);
 		
 	}	
